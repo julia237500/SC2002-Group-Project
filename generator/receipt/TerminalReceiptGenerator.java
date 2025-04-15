355b7c65182bcf7e8f64076b9cdd846010b96b25
PotatoKnight24 <wangjuncheng24@gmail.com>
2025-04-15 11:38:38 +0800

    Terminal Receipt Generator

--- generator/receipt/TerminalReceiptGenerator.java
+++ generator/receipt/TerminalReceiptGenerator.java
@@ -1,5 +1,6 @@
 package generator.receipt;
 
+import config.MaritalStatus;
 import model.Application;
 
 public class TerminalReceiptGenerator implements ReceiptGenerator{
@@ -10,5 +11,29 @@ public class TerminalReceiptGenerator implements ReceiptGenerator{
         // Dont need to check if it is booked or not
         // Just print the details
         // You can decide the format
+        String applicantName = application.getApplicant().getName();
+        String applicantNRIC = application.getApplicant().getNRIC();
+        Integer applicantAge = application.getApplicant().getAge());
+        MaritalStatus applicantMaritalStatus = application.getApplicant().getMaritalStatus();
+
+        String projectName = application.getBtoProject().getName();
+        String flatType = application.getFlatType().toString();
+        String createdAt = application.getCreatedAt().toString();
+
+        String receipt = """
+                ======================================
+                BTO APPLICATION RECEIPT
+                ======================================
+                Applicant Name: %s
+                Applicant NRIC: %s
+                Applicant Age: %d
+                Marital Status: %s
+                Project Name:   %s
+                Flat Type:      %s
+                Application Date: %s
+                ======================================
+                """.formatted(applicantName,applicantNRIC,applicantAge,applicantMaritalStatus,projectName,flatType,createdAt);
+        System.out.println(receipt);
+
     }
 }
