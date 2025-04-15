6866182cb1e1e0829e759a3b8f671f9e898c36e1
julia237500 <julial.levelup@gmail.com>
2025-04-15 23:12:57 +0800

    Update FormField.java

--- config/FormField.java
+++ config/FormField.java
@@ -1,21 +1,53 @@
 package config;
 
+/**
+ * Represents the different form fields used in the HDB application system.
+ * This enum serves as a centralized reference for all form field identifiers,
+ * ensuring consistent naming across the entire application.
+ */
 public enum FormField {
+    /** NRIC number field */
     NRIC,
+
+    /** User password field */
     PASSWORD,
+
+    /** Password confirmation field */
     CONFIRM_PASSWORD,
 
+    /** User's full name field */
     NAME,
+
+    /** Neighborhood selection field for BTO projects */
     NEIGHBORHOOD,
+
+    /** Field for number of available 2-Room flats */
     TWO_ROOM_FLAT_NUM,
+
+    /** Field for price of 2-Room flats */
     TWO_ROOM_FLAT_PRICE,
+
+    /** Field for number of available 3-Room flats */
     THREE_ROOM_FLAT_NUM,
+
+    /** Field for price of 3-Room flats */
     THREE_ROOM_FLAT_PRICE,
+
+    /** Field for BTO project application opening date */
     OPENING_DATE,
+
+    /** Field for BTO project application closing date */
     CLOSING_DATE,
+
+    /** Field for maximum number of HDB officers allowed for a project */
     HBD_OFFICER_LIMIT,
 
+    /** Field for enquiry subject */
     SUBJECT,
+
+    /** Field for enquiry content */
     ENQUIRY,
+
+    /** Field for enquiry reply */
     REPLY
 }
