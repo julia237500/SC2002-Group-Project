6b0bf93dd55e1b4ff3087cba4e4719088839bfa3
julia237500 <julial.levelup@gmail.com>
2025-04-16 09:29:43 +0800

    Update AbstractDefaultController.java

--- controller/AbstractDefaultController.java
+++ controller/AbstractDefaultController.java
@@ -4,13 +4,28 @@ import config.ResponseStatus;
 import service.ServiceResponse;
 import view.interfaces.MessageView;
 
+/**
+ * Provides common functionality for displaying service responses.
+ * This class handles the standard workflow of showing success/error messages from service operations.
+ */
 public class AbstractDefaultController {
     protected MessageView messageView;
 
+    /**
+     * Constructs an AbstractDefaultController with the specified message view.
+     * 
+     * @param messageView the view component used for displaying messages
+     */
     protected AbstractDefaultController(MessageView messageView){
         this.messageView = messageView;
     }
 
+    /**
+     * Displays a service response using the configured message view.
+     * Shows success or error messages based on the response status.
+     * 
+     * @param serviceResponse the service response to display
+     */
     protected void defaultShowServiceResponse(ServiceResponse<?> serviceResponse){
         if(serviceResponse.getResponseStatus() == ResponseStatus.SUCCESS){
             messageView.success(serviceResponse.getMessage());
@@ -20,3 +35,4 @@ public class AbstractDefaultController {
         }
     }
 }
+    
