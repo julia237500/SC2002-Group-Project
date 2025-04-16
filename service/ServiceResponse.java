512dc8de10b1a46b52a1c2ddb65d728b6a8d403a
julia237500 <julial.levelup@gmail.com>
2025-04-16 09:38:55 +0800

    Update ServiceResponse.java

--- service/ServiceResponse.java
+++ service/ServiceResponse.java
@@ -2,43 +2,92 @@ package service;
 
 import config.ResponseStatus;
 
+/**
+ * A generic container for service operation responses, including status, message, and data.
+ * 
+ * @param <T> the type of data contained in this response
+ */
 public class ServiceResponse<T> {
     private final ResponseStatus responseStatus;
     private final String message;
     private final T data;
 
+    /**
+     * Constructs a response with only a status (empty message and null data).
+     * 
+     * @param responseStatus the status of the service operation
+     */
     public ServiceResponse(ResponseStatus responseStatus){
         this.responseStatus = responseStatus;
         this.message = "";
         this.data = null;
     }
 
+    /**
+     * There is constructor overloading involved in this class, where there's
+     * more than one constructor with different parameter lists. 
+     */
+
+    /**
+     * Constructs a response with status and data (empty message).
+     * 
+     * @param responseStatus the status of the service operation
+     * @param data the response data
+     */
     public ServiceResponse(ResponseStatus responseStatus, T data){
         this.responseStatus = responseStatus;
         this.message = "";
         this.data = data;
     }
 
+    /**
+     * Constructs a response with status and message (null data).
+     * 
+     * @param responseStatus the status of the service operation 
+     * @param message human-readable response message (empty string if null)
+     */
     public ServiceResponse(ResponseStatus responseStatus, String message){
         this.responseStatus = responseStatus;
         this.message = message;
         this.data = null;
     }
 
+    /**
+     * Constructs a complete response with status, message, and data.
+     * 
+     * @param responseStatus the status of the service operation 
+     * @param message human-readable response message
+     * @param data the response data 
+     */
     public ServiceResponse(ResponseStatus responseStatus, String message, T data){
         this.responseStatus = responseStatus;
         this.message = message;
         this.data = data;
     }
 
+    /**
+     * Gets the status of the service operation.
+     * 
+     * @return the response status
+     */
     public ResponseStatus getResponseStatus() {
         return responseStatus;
     }
 
+    /**
+     * Gets the human-readable response message.
+     * 
+     * @return the message (empty string if none was provided)
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Gets the response data payload.
+     * 
+     * @return the data (may be null if no data was provided)
+     */
     public T getData() {
         return data;
     }
