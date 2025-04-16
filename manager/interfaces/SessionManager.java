5b1ea5cf5d2bea7b9a11e4f760f5df453b012742
julia237500 <julial.levelup@gmail.com>
2025-04-16 22:29:23 +0800

    Update SessionManager.java

--- manager/interfaces/SessionManager.java
+++ manager/interfaces/SessionManager.java
@@ -2,8 +2,32 @@ package manager.interfaces;
 
 import model.User;
 
+/**
+ * The {@code SessionManager} interface defines the contract for
+ * managing user session state within the application.
+ * 
+ * It handles setting, retrieving, and resetting the currently logged-in user.
+ */
 public interface SessionManager {
+
+    /**
+     * Sets the current active user for the session.
+     * This should be called after a successful login.
+     *
+     * @param user The {@code User} object representing the logged-in user.
+     */
     void setUser(User user);
+
+    /**
+     * Retrieves the current user in the session.
+     * 
+     * @return The {@code User} object for the currently logged-in user
+     */
     User getUser();
+
+    /**
+     * Resets the current session by clearing the user data.
+     * Typically used during logout.
+     */
     void resetUser();
 }
