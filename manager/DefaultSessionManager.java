5ada77ec230b69ccaee74150d21b5f596ee19fe3
julia237500 <julial.levelup@gmail.com>
2025-04-16 17:36:51 +0800

    Update DefaultSessionManager.java

--- manager/DefaultSessionManager.java
+++ manager/DefaultSessionManager.java
@@ -3,17 +3,35 @@ package manager;
 import manager.interfaces.SessionManager;
 import model.User;
 
+/**
+ * Default implementation of {@link SessionManager} that handles storing and 
+ * resetting the current user session in memory.
+ */
 public class DefaultSessionManager implements SessionManager{
     private User user;
 
+    /**
+     * Sets the current logged-in user for the session.
+     *
+     * @param user the {@link User} to be set in the session
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * Retrieves the currently logged-in user from the session.
+     *
+     * @return the current {@link User} in session
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Clears the current session by resetting the user to {@code null}.
+     * Typically used during logout.
+     */
     public void resetUser() {
         this.user = null;
     }
