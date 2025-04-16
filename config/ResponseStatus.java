2248d1910d5033a401e603f954b5f20fb831f438
julia237500 <julial.levelup@gmail.com>
2025-04-16 08:40:16 +0800

    Update

--- config/ResponseStatus.java
+++ config/ResponseStatus.java
@@ -1,7 +1,23 @@
 package config;
 
+/**
+ * Represents the possible response statuses.
+ * This enum provides only programmatic constants.
+ */
 public enum ResponseStatus {
+
+    /**
+     * Response is a success
+     */
     SUCCESS,
+
+    /**
+     * Response is an error
+     */
     ERROR,
+
+    /**
+     * Response is a failure 
+     */
     FAILURE,
 }
