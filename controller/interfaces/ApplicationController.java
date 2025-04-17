74112866cd648c3a0b660d454781b2eb491901be
julia237500 <julial.levelup@gmail.com>
2025-04-17 18:14:36 +0800

    Update ApplicationController.java

--- controller/interfaces/ApplicationController.java
+++ controller/interfaces/ApplicationController.java
@@ -3,6 +3,20 @@ package controller.interfaces;
 import config.FlatType;
 import model.BTOProject;
 
+
+/**
+ * Interface for handling user interactions related to housing applications.
+ * 
+ * <p>This interface defines the core action of submitting an application
+ * for a specific flat type under a BTO project.</p>
+ */
 public interface ApplicationController {
+
+    /**
+     * Submits an application for a given flat type under the specified BTO project.
+     *
+     * @param btoProject The BTO project to apply under.
+     * @param flatType   The type of flat the user is applying for.
+     */
     void addApplication(BTOProject btoProject, FlatType flatType);
 }
