ff83677107c4272f4a50094a3576b266e4db2969
julia237500 <julial.levelup@gmail.com>
2025-04-17 14:44:50 +0800

    Update MessageView.java

--- view/interfaces/MessageView.java
+++ view/interfaces/MessageView.java
@@ -1,8 +1,36 @@
 package view.interfaces;
 
+/**
+ * Interface for displaying different types of messages to the user.
+ *
+ */
 public interface MessageView {
+
+    /**
+     * Displays an informational message to the user.
+     *
+     * @param message The informational message to be displayed.
+     */
     void info(String message);
+
+    /**
+     * Displays a success message to the user.
+     *
+     * @param message The success message to be displayed.
+     */
     void success(String message);
+
+    /**
+     * Displays a non-critical error message (e.g., invalid input).
+     *
+     * @param message The error message to be displayed.
+     */
     void error(String message);
+
+    /**
+     * Displays a critical failure message (e.g., operation failed).
+     *
+     * @param message The failure message to be displayed.
+     */
     void failure(String message);
 }
