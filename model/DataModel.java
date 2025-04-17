6ade2c6a7911cd49f0e3a51c75b528ba45cdf422
julia237500 <julial.levelup@gmail.com>
2025-04-17 09:12:27 +0800

    Update DataModel.java

--- model/DataModel.java
+++ model/DataModel.java
@@ -1,5 +1,19 @@
 package model;
 
+/**
+ * Implemented by other model classes 
+ * Represents a generic data model with a primary key.
+ * This interface is implemented by classes that represent
+ * persistent data entities (e.g., applications, users, projects).
+ * It provides a method to retrieve the unique identifier (primary key)
+ * for each instance of the model.
+ */
 public interface DataModel {
+
+    /**
+     * Returns the primary key (unique identifier) of the data model instance.
+     *
+     * @return a {@code String} representing the primary key
+     */
     String getPK();
 }
