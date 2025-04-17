d5b4bdffc101a096cfbb708c34a76561b54efddf
julia237500 <julial.levelup@gmail.com>
2025-04-18 00:24:25 +0800

    Update DefaultApplicationController.java

--- controller/DefaultApplicationController.java
+++ controller/DefaultApplicationController.java
@@ -9,10 +9,25 @@ import service.ServiceResponse;
 import service.interfaces.ApplicationService;
 import view.interfaces.MessageView;
 
+/**
+ * Default implementation of the {@link ApplicationController} interface.
+ * <p>
+ * This controller is responsible for handling user-driven logic related to 
+ * applications for BTO projects. It delegates core business logic to the 
+ * {@link ApplicationService} and uses the {@link SessionManager} to retrieve 
+ * the currently logged-in user.
+ */
 public class DefaultApplicationController extends AbstractDefaultController implements ApplicationController{
     private ApplicationService applicationService;
     private SessionManager sessionManager;
 
+    /**
+     * Constructs a new {@code DefaultApplicationController}.
+     *
+     * @param applicationService the service that handles application-related logic
+     * @param messageView        the view used to display messages to the user
+     * @param sessionManager     the session manager that provides user session data
+     */
     public DefaultApplicationController(ApplicationService applicationService, MessageView messageView, SessionManager sessionManager){
         super(messageView);
 
@@ -20,6 +35,12 @@ public class DefaultApplicationController extends AbstractDefaultController impl
         this.sessionManager = sessionManager;
     }
 
+    /**
+     * Adds an application for the given BTO project and flat type using the currently logged-in user.
+     *
+     * @param btoProject the BTO project the user is applying for
+     * @param flatType   the type of flat the user is applying for
+     */
     @Override
     public void addApplication(BTOProject btoProject, FlatType flatType) {
         User user = sessionManager.getUser();
