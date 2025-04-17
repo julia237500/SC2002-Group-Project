064c8fe813ef8755c83a961e96c063444ddd482a
julia237500 <julial.levelup@gmail.com>
2025-04-17 08:53:45 +0800

    Update CSVField.java

--- model/CSVField.java
+++ model/CSVField.java
@@ -3,8 +3,25 @@ package model;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.RUNTIME) // Make sure the annotation is available at runtime
+/**
+ * Annotation to mark a field as part of a CSV-serializable data model.
+ * <p>
+ * This annotation is used to map fields in a model class to specific
+ * positions (indices) in a CSV file.
+ * </p>
+ *
+ * <p>
+ * The annotation is retained at runtime to allow reflective access,
+ * typically used during dynamic CSV parsing and object instantiation.
+ * </p>
+ */
+@Retention(RetentionPolicy.RUNTIME) // Makes sure the annotation is available at runtime
 public @interface CSVField {
     int index(); // This will represent the position of the field in the CSV
+    /**
+     * Specifies whether this field is a foreign key reference to another object.
+     *
+     * @return {@code true} if this field is a foreign key, {@code false} otherwise
+     */
     boolean foreignKey() default false;
 }
