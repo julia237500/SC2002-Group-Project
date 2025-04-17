8305521dcb434673ac2fa7825c8c6b87df04b08c
julia237500 <julial.levelup@gmail.com>
2025-04-17 12:12:29 +0800

    Update BTOProjectView.java

--- view/interfaces/BTOProjectView.java
+++ view/interfaces/BTOProjectView.java
@@ -4,7 +4,34 @@ import java.util.List;
 
 import model.BTOProject;
 
+/**
+ * This interface fits into the Model-View-Controller (MVC) pattern, 
+ * It helps decouple our UI logic from the data and business logic, 
+ * making our code more modular.
+ * Separation of Concerns, Single Responsibility Principle: 
+ * - View classes are only responsible for display and UI logic 
+ */
+
+/**
+ * Interface for displaying BTO project-related data in the view layer.
+ * <p>
+ * This interface defines the contract for how BTO project information
+ * should be presented to the user, whether as a list or as a detailed
+ * view of a single project.
+ */
 public interface BTOProjectView {
+
+    /**
+     * Displays a list of BTO projects to the user.
+     *
+     * @param btoProjects A list of {@link BTOProject} objects to be shown.
+     */
     void showBTOProjects(List<BTOProject> btoProjects);
+
+    /**
+     * Displays the details of a single BTO project.
+     *
+     * @param btoProject The {@link BTOProject} object to be shown.
+     */
     void showBTOProject(BTOProject btoProject);
 }
