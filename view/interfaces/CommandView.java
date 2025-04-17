f21235043d857be3ec996fdbc9fb319b69a40ebd
julia237500 <julial.levelup@gmail.com>
2025-04-17 12:15:38 +0800

    Update CommandView.java

--- view/interfaces/CommandView.java
+++ view/interfaces/CommandView.java
@@ -4,7 +4,25 @@ import java.util.Map;
 
 import command.Command;
 
+/**
+ * Interface for displaying a list of commands to the user and retrieving their selection.
+ * <p>
+ * This interface supports the separation of concerns by decoupling the user interface
+ * logic from the underlying command execution logic.
+ */
 public interface CommandView {
+    /**
+     * Displays a list of selectable commands to the user with a specified title.
+     *
+     * @param title the title or heading of the command selection menu
+     * @param commands a map of integer options to their corresponding {@link Command} objects
+     */
     void showCommandSelection(String title, Map<Integer, Command> commands);
+
+    /**
+     * Retrieves the user's selected command option.
+     *
+     * @return the integer corresponding to the user's selected command
+     */
     int getCommandSelection();
 }
