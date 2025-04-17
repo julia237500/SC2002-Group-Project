147d10c94f9c55550dbecca603270310e1fd0150
julia237500 <julial.levelup@gmail.com>
2025-04-17 14:51:02 +0800

    Update TerminalBTOProjectView.java

--- view/terminal/TerminalBTOProjectView.java
+++ view/terminal/TerminalBTOProjectView.java
@@ -6,7 +6,19 @@ import model.BTOProject;
 import model.User;
 import view.interfaces.BTOProjectView;
 
+
+/**
+ * Terminal-based implementation of the {@link BTOProjectView} interface.
+ *
+ */
 public class TerminalBTOProjectView extends AbstractTerminalView implements BTOProjectView{
+
+    /**
+     * Displays a list of BTO projects in the terminal.
+     * Each project is shown with an index and its name.
+     *
+     * @param btoProjects the list of {@link BTOProject} instances to display.
+     */
     public void showBTOProjects(List<BTOProject> btoProjects){
         showTitle("BTO Project List");
 
@@ -15,6 +27,14 @@ public class TerminalBTOProjectView extends AbstractTerminalView implements BTOP
         }
     }
 
+    /**
+     * Displays detailed information for a single BTO project.
+     * <p>
+     * Includes the project's string representation and the names of HDB officers assigned to it.
+     * </p>
+     *
+     * @param btoProject the {@link BTOProject} instance to display.
+     */
     public void showBTOProject(BTOProject btoProject){
         showTitle("BTO Project Detail");
         System.out.println(btoProject.toString());
