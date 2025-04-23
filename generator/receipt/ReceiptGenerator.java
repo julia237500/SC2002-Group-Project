686f2885e8e88c965ea60a54382008ee3776ede9
julia237500 <julial.levelup@gmail.com>
2025-04-23 23:17:08 +0800

    Update ReceiptGenerator.java

--- generator/receipt/ReceiptGenerator.java
+++ generator/receipt/ReceiptGenerator.java
@@ -2,6 +2,18 @@ package generator.receipt;
 
 import model.Application;
 
+/**
+ * The {@code ReceiptGenerator} interface defines a contract for generating receipts
+ * based on a given {@link Application}.
+ * 
+ * <p>Implementations of this interface are responsible for the formatting and output
+ * of a receipt</p>
+ */
 public interface ReceiptGenerator {
+    /**
+     * Generates a receipt for the specified {@link Application}.
+     *
+     * @param application the application for which the receipt is to be generated
+     */
     void generateReceipt(Application application);
 }
